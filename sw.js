9b769a0a7de89d3fa3aed9f1597bd8bc1b42ed92
nateparish-cyber <nate.parish@gmail.com>
2026-07-17 10:58:14 -0700

    Post 9: add cover image (empty chair beside open door, watercolor)
    Also refreshes landing "Latest from the blog" to Post 9/8/7 and bumps
    SW cache v25 -> v26 so cached browsers fetch the new HTML on reload.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'spc-v25';
+const CACHE = 'spc-v26';
 const PRECACHE = ['/', '/index.html', '/icon.svg', '/manifest.json', '/spc-icon.png', '/apple-touch-icon.png', '/icon-192.png'];
 
 self.addEventListener('install', e => {
